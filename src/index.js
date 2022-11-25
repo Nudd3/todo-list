b8cdf039eee0511368ef6c11d6cf49c3a7d52579
Noak Pettersson <noake12@gmail.com>
2022-11-25 19:19:35 +0100

    tested manually adding a new list and it's handling when pressed. Task are shown, as are addTaskBtn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import './styles.css'
-import UI from './modules/ui'
 import DomManager from './modules/dom-manager'
 
 document.addEventListener('DOMContentLoaded', () => {
-  DomManager.displayLists();
   //const ui = new UI();
   //ui.load();
   //new UI().load();
+  DomManager.init();
 })
--- src/modules/dom-manager.js
+++ src/modules/dom-manager.js
@@ -5,13 +5,28 @@ import TodoList from './todo-list';
 import Storage from './storage.js';
 
 export default class DomManager {
+
+  static init() {
+    console.log('hello');
+    DomManager.displayLists();
+  }
+
   /** This method should only display lists from storage on load up
    *  New lists will be displayed after creation from addList
    */
   static displayLists() {
+    /** 
+     * Read lists from storage
+     * and call createListDiv with them as arguments
+    */
+
+    /** Below stuff are for testing */
     const lists = document.getElementById('lists');
-    const newTodoList = new TodoList('Clean!', 'Just do it!', 'none', 'none');
-    lists.appendChild(this.createListDiv(newTodoList));
+    const exampleTodoList = DomManager.createExampleTodo();
+    
+    lists.appendChild(DomManager.createListDiv(exampleTodoList));
+    /** End of testing */
+
   }
 
   static createListDiv(list) {
@@ -19,10 +34,8 @@ export default class DomManager {
     const listDiv = document.createElement('div');
     listDiv.classList.add('list');
     listDiv.textContent = list.getTitle();
-    // handle listeners(listDiv, list);
-    // when a listDiv is pressed
-    // - its tasks should be shown
-    // - the addNewTask btn should be shown
+    DomManager.handleListListeners(list, listDiv);
+
     return listDiv;
   }
 
@@ -37,4 +50,42 @@ export default class DomManager {
   static createTaskDiv() {}
 
   static addTask() {}
+
+
+
+  // Handlers
+  static handleListListeners(list, listDiv) {
+    // when a listDiv is pressed
+    // - its tasks should be shown
+    // - the addNewTask btn should be shown
+    // show add task button
+    const taskSection = document.getElementById('listTasks');
+    listDiv.addEventListener('click', () => {
+
+
+      // Display tasks
+      list.getTasks().forEach(task => {
+        const newTask = document.createElement('div');
+        newTask.classList.add('list');
+        newTask.textContent = task.getTitle();
+        taskSection.appendChild(newTask);
+      });
+
+      // Display the add task button
+      const addTaskButton = document.getElementById('addTaskButton');
+      addTaskButton.classList.add('show');
+
+    });
+  }
+
+
+  static createExampleTodo() {
+    const newTodoList = new TodoList('Clean!', 'Just do it!', 'none', 'none');
+    const task1 = new Task('shower', 'has to be done', 'low', 'none');
+    const task2 = new Task('toilet', 'discusting', 'high', 'none');
+    newTodoList.addTask(task1);
+    newTodoList.addTask(task2);
+    return newTodoList;
+  }
+
 }
